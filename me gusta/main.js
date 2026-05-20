13981b96506043ba1e8c30dc844782c2bcc1e622
katysepulveda <1234katy00@gmail.com>
2026-05-19 21:58:09 -0400

    Delete me gusta directory

--- me gusta/main.js
+++
@@ -1,8 +0,0 @@
-let btn1 =document.querySelector("#btn1")
-let contadorLikes = 0;
-btn3.onclick = function () {
-    contadorLikes++; 
-    btn1.innerHTML = contadorLikes + " me gusta"; 
-    console.log(contadorLikes); 
-    alert("Golden Retriever was liked");
-}
